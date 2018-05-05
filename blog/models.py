688f41bb6b35265203a2bfde5733d8084a26244d
zed <heik@vip.qq.com>
2018-05-05 23:29:01 +0800

    修正文章正确阅读数值

--- blog/models.py
+++ blog/models.py
@@ -30,12 +30,17 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def increase_view(self):
+        self.views += 1
+        self.save(update_fields=['views'])
 
     class Meta:
         ordering = ['-created_time']
