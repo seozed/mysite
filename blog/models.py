0fc7f3f3d611e034273d50e93302d6ae7afa50ea
zed <heik@vip.qq.com>
2018-05-05 23:23:10 +0800

    现在可以显示每篇文章对应的评论数了

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+
+
 # Create your models here.
 
 class Category(models.Model):
-
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 
 class Tag(models.Model):
-
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-class Post(models.Model):
 
+class Post(models.Model):
     title = models.CharField(max_length=70)
 
     body = models.TextField()
@@ -35,3 +36,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        ordering = ['-created_time']
